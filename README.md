41dd5d9ea9075ffdeb51bcd82e22e69c0b138e2e
Soorria Saruva <soorria.ss@gmail.com>
2022-12-27 23:06:16 +1100

    initial port

--- README.md
+++ README.md
@@ -1,36 +1,29 @@
 <p>
-  <img width="100%" src="https://assets.solidjs.com/banner?type={{name_of_lib}}&background=tiles&project=%20" alt="{{name_of_lib}}">
+  <img width="100%" src="https://assets.solidjs.com/banner?type=solid-dropzone&background=tiles&project=%20" alt="solid-dropzone">
 </p>
 
-# {{name_of_lib}}
+# solid-dropzone
 
 [![pnpm](https://img.shields.io/badge/maintained%20with-pnpm-cc00ff.svg?style=for-the-badge&logo=pnpm)](https://pnpm.io/)
 
-{{desc_of_lib}}
+Drag and drop file input library for SolidJS.
 
-> **Note** After using this template, you have to search and replace all `{{name_of_lib}}` and similar strings
-> with appropriate texts.
->
-> `{{name_of_lib}}` should be a **kebab-case** string representing the name of you monorepo.
->
-> `{{desc_of_lib}}` should be a **Normal case** string with the description of the repository.
->
-> `{{me}}` should be a **kebab-case** string from your profile URL.
+> **Note**: This is a port of [`react-dropzone`](https://github.com/react-dropzone/react-dropzone)
 
 ## Quick start
 
 Install it:
 
 ```bash
-npm i {{name_of_lib}}
+npm i solid-dropzone
 # or
-yarn add {{name_of_lib}}
+yarn add solid-dropzone
 # or
-pnpm add {{name_of_lib}}
+pnpm add solid-dropzone
 ```
 
 Use it:
 
 ```tsx
-import {{name_of_lib}} from '{{name_of_lib}}'
+import { createDropzone } from 'solid-dropzone'
 ```
--- dev/App.tsx
+++ dev/App.tsx
@@ -1,30 +1,95 @@
-import type { Component } from "solid-js";
-import logo from "./logo.svg";
-import styles from "./App.module.css";
-import { Hello } from "../src";
+import { Component, splitProps, For, mergeProps, Show, ComponentProps } from 'solid-js'
+import { createDropzone, CreateDropzoneProps, DropEvent, FileRejection } from '../src'
+
+const Dropzone = (props: Partial<CreateDropzoneProps> & { title: string }) => {
+  const [local, dropzoneProps] = splitProps(props, ['title'])
+  const dropzone = createDropzone(
+    mergeProps(dropzoneProps, {
+      onDrop: (files: File[], rejections: FileRejection[], event: DropEvent | null) => {
+        props.onDrop?.(files, rejections, event)
+      },
+    }),
+  )
+
+  const hasOutput = () => dropzone.acceptedFiles.length > 0 || dropzone.fileRejections.length > 0
+
+  return (
+    <div class="space-y-6">
+      <h2 class="font-bold text-2xl">{local.title}</h2>
+      <div
+        {...dropzone.getRootProps({
+          class: 'rounded-xl border-2 border-purple-700 min-h-[8rem] relative overflow-hidden',
+        })}
+      >
+        <Show when={dropzone.isDragActive}>
+          <div class="absolute inset-0 grid place-items-center bg-white">drop files here</div>
+        </Show>
+
+        <Show when={!hasOutput() && !dropzone.isDragActive}>
+          <div class="absolute inset-0 grid place-items-center">click or drag files to upload</div>
+        </Show>
+
+        <Show when={hasOutput()}>
+          <div class="grid gap-1 p-4 grid-cols-2">
+            <div>
+              <p class="font-bold text-lg">Accepted Files</p>
+              <ul class="list-disc pl-4">
+                <For each={dropzone.acceptedFiles} fallback={<li>No files accepted :(</li>}>
+                  {file => <li>{file.name}</li>}
+                </For>
+              </ul>
+            </div>
+            <div>
+              <p class="font-bold text-lg">Rejected Files</p>
+              <ul class="list-disc pl-4">
+                <For each={dropzone.fileRejections} fallback={<li>No files rejected :)</li>}>
+                  {rejection => (
+                    <li>
+                      {rejection.file.name}:{' '}
+                      <span class="text-sm">
+                        {rejection.errors.map(e => `${e.code} - ${e.message}`).join(', ')}
+                      </span>
+                    </li>
+                  )}
+                </For>
+              </ul>
+            </div>
+          </div>
+        </Show>
+
+        <input {...dropzone.getInputProps({ id: 'input-0' })} />
+      </div>
+    </div>
+  )
+}
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <h1>
-          <Hello></Hello>
-        </h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
-};
+    <div class="max-w-screen-md mx-auto space-y-8 mt-8 px-8 pb-24">
+      <h1 class="font-bold text-4xl">Solid Dropzone</h1>
 
-export default App;
+      <Dropzone title="Nothing special" />
+      <Dropzone title=".png only" accept=".png" />
+      <Dropzone
+        title="3 files max"
+        maxFiles={3}
+        onDrop={(...args) => console.log(args)}
+        onError={err => console.error(err)}
+      />
+      <Dropzone
+        title="1 file only (multiple=false)"
+        multiple={false}
+        onDrop={(...args) => console.log(args)}
+        onError={err => console.error(err)}
+      />
+      <Dropzone
+        title="1 file only (maxFiles=1)"
+        maxFiles={1}
+        onDrop={(...args) => console.log(args)}
+        onError={err => console.error(err)}
+      />
+    </div>
+  )
+}
+
+export default App
